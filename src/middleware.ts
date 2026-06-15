c71ef5a2bef72c6dc2713a5a9e3b5cec1be92186
somaya-77 <alkhtybsmytadl@gmail.com>
2026-06-16 01:56:04 +0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-
+  console.log("MIDDLEWARE RUNNING");
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET
