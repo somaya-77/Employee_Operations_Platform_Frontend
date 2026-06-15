8c2824e8157ea6463731601a5f780ffde38b845a
somaya-77 <alkhtybsmytadl@gmail.com>
2026-06-16 02:52:00 +0300

    auth coo

--- src/auth.ts
+++ src/auth.ts
@@ -7,7 +7,19 @@ export const authOptions: NextAuthOptions = {
         signIn: "/auth/login",
         error: "/auth/login",
     },
-
+  cookies: {
+    sessionToken: {
+      name: process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token",
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
     providers: [
         Credentials({
             name: "Credentials",
